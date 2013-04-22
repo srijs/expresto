2b3bea470deca018c42ffc99d983d1a971a61d09
Sam Rijs <sam@dev.local>
2013-04-22 22:17:34 +0200

    Enable post resources.

--- resource.js
+++ resource.js
@@ -11,12 +11,34 @@ function validator (req, value, next) {
 
 function transactor (req, query, value, next) {
   if (typeof value.transaction === 'function') {
-    return value.transaction(req, query, function (err, data) {
+    value.transaction(req, query, function (err, transaction) {
+      var data, result;
       if (err) return next(err);
-      else     return next(null, data);
+      if (transaction.collection) {
+        if (transaction.find) {
+          data = transaction.collection.find(transaction.find);
+          if (transaction.sort)  data = data.sort(transaction.sort);
+          if (transaction.limit) data = data.limit(transaction.limit);
+          result = [];
+          data.each(function (err, item) {
+            if (err)   return next(err);
+            if (!item) return next(null, result);
+            result.push(item);
+          });
+        } else if (transaction.insert) {
+          transaction.collection.insert(transaction.insert, function (err, docs) {
+            if (err) return next(err);
+            else     return next(null, docs);
+          });
+        } else {
+          return next(null, null);
+        }
+      } else {
+        return next(null, null);
+      }
     });
   } else {
-    next(null, null);
+    return next(null, null);
   }
 }
 
@@ -27,8 +49,22 @@ function formatter (req, data, value, next) {
       else     return next(null, formatted);
     });
   } else {
-    next(null, data);
+    return next(null, data);
   }
+}
+
+function chain (req, res, value, status){
+  validator(req, value, function (err, query) {
+    if (err)    return res.send(500);
+    if (!query) return res.send(400);
+    transactor(req, query, value, function (err, data) {
+      if (err) return res.send(500);
+      formatter(req, data, value, function (err, formatted) {
+        if (err) return res.send(500);
+        res.status(status).json(formatted);
+      });
+    });
+  });
 }
 
 module.exports = function exResource (app) {
@@ -36,23 +72,15 @@ module.exports = function exResource (app) {
   return function (route, resource) {
 
     if (typeof resource.get === 'object') {
-
       app.get(route, function (req, res) {
-
-        validator(req, resource.get, function (err, query) {
-          if (err)    return res.send(500);
-          if (!query) return res.send(400);
-          transactor(req, query, resource.get, function (err, data) {
-            if (err) return res.send(500);
-            formatter(req, data, resource.get, function (err, formatted) {
-              if (err) return res.send(500);
-              res.json(formatted);
-            });
-          });
-        });
-
+        chain(req, res, resource.get, 200);
       });
+    }
 
+    if (typeof resource.post === 'object') {
+      app.post(route, function (req, res) {
+        chain(req, res, resource.post, 201);
+      });
     }
 
   };
